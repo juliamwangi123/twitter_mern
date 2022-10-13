16d4b3b0b4e05a1a148c847d5e359735a4106f89
Julie <juliamwangi123>
2022-10-13 11:30:09 +0300

    .

--- Backend/models/authModel.js
+++ Backend/models/authModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+
+
+
+
 
 
 const userSchema = new mongoose.Schema({
